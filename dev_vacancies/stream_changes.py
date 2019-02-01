12e5c000576a98137b1ad00dbae339e6b92572a6
KonstantinMoskalenko <kmoskalenko@new.com>
2019-02-01 18:45:16 +0300

    stream changes

--- dev_vacancies/stream_changes.py
+++ dev_vacancies/stream_changes.py
@@ -7,9 +7,11 @@ db = client.vacancies
 col = db.vacancies_test
 
 try:
-    with col.watch(
-            [{'$match': {'operationType': 'insert'}}]) as stream:
+    with db.vacancies_test.watch([{'$match': {'operationType': 'insert', 'fullDocument.message':{'$exists':0}}}]) as stream:
         for insert_change in stream:
             print(insert_change)
+            db.test.insert({'message':'Hello from python!_2'})
+            # db.test.insert(insert_change["fullDocument"])
+            # db.another.update({"_id": insert_change["documentKey"]["_id"]}, {"$set":{"from_python": "hello"}})
 except pymongo.errors.PyMongoError:
     pass
