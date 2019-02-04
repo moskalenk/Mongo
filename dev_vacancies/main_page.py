9ddcd11954da76619f57307625b09eb2b7579137
KonstantinMoskalenko <kmoskalenko@new.com>
2019-02-04 19:08:15 +0300

    change getting data + add method for update data in db

--- dev_vacancies/main_page.py
+++ dev_vacancies/main_page.py
@@ -1,30 +1,38 @@
 import requests
 from bs4 import BeautifulSoup
 
-MAIN_URL = 'https://jobs.dev.by'
 
-vacancies_page = requests.get('https://jobs.dev.by/?search-jobs[user_type_of_activity]=3&search-jobs-filter=true')
+class Common:
 
-soup = BeautifulSoup(vacancies_page.text, 'html.parser')
-list_jobs = soup.find(class_='list-jobs')
-all_h3 = list_jobs.find_all('h3')
+    MAIN_URL = 'https://jobs.dev.by'
 
-company_name = list_jobs.find_all('strong')
-# company_name = list_jobs.find('strong')
-# print(company_name.text)
+    vacancies_page = requests.get('https://jobs.dev.by/?search-jobs[user_type_of_activity]=3&search-jobs-filter=true')
 
+    soup = BeautifulSoup(vacancies_page.text, 'html.parser')
+    list_jobs = soup.find(class_='list-jobs')
+    all_h3 = list_jobs.find_all('h3')
 
-def get_data():
-    for h3 in all_h3:
-        for company in company_name:
-            name = company.text
+    company_name = list_jobs.find_all('strong')
 
+    def get_summary_and_link(self):
+        for h3 in self.all_h3:
             a = h3.find('a')
-            link_to_vacancy = MAIN_URL + a['href']
+            link_to_vacancy = self.MAIN_URL + a['href']
             text_for_vacancy = a.text
+            yield text_for_vacancy, link_to_vacancy
 
-            yield f'{link_to_vacancy} - {text_for_vacancy} - {name.strip()}'
+    def get_company_name(self):
+        for company in self.company_name:
+            name = company.text
+            yield name.strip()
+
+    def get_data(self):
+        for el in map(lambda x, y: [x, y], self.get_summary_and_link(), self.get_company_name()):
+            yield el[0][0], el[0][1], el[1]
 
 
-res = get_data()
-print(list(res))
+
+# c = Common()
+# r = c.get_data()
+# # r = c.tmp()
+# print(list(r))
--- dev_vacancies/stream_changes.py
+++ dev_vacancies/stream_changes.py
@@ -1,17 +1,26 @@
 from pymongo import MongoClient
 import pymongo.errors
+from main_page import Common
 
 client = MongoClient()
+vac_obj = Common()
 
 db = client.vacancies
-col = db.vacancies_test
+col = db.vacancies_tmp
 
-try:
-    with db.vacancies_test.watch([{'$match': {'operationType': 'insert', 'fullDocument.message':{'$exists':0}}}]) as stream:
-        for insert_change in stream:
-            print(insert_change)
-            db.test.insert({'message':'Hello from python!_2'})
-            # db.test.insert(insert_change["fullDocument"])
-            # db.another.update({"_id": insert_change["documentKey"]["_id"]}, {"$set":{"from_python": "hello"}})
-except pymongo.errors.PyMongoError:
-    pass
+count = 0
+
+for vac in vac_obj.get_data():
+  text_for_vacancy, link_to_vacancy, company_name = vac
+
+  col.update_one({'company': company_name}, {'$addToSet': {'vacancies': {'summary': text_for_vacancy, 'link': link_to_vacancy}}}, True)
+
+# try:
+#     with db.vacancies_test.watch([{'$match': {'operationType': 'insert', 'fullDocument.message':{'$exists':0}}}]) as stream:
+#         for insert_change in stream:
+#             print(insert_change)
+#             db.test.insert({'message':'Hello from python!_2'})
+#             # db.test.insert(insert_change["fullDocument"])
+#             # db.another.update({"_id": insert_change["documentKey"]["_id"]}, {"$set":{"from_python": "hello"}})
+# except pymongo.errors.PyMongoError:
+#     pass
